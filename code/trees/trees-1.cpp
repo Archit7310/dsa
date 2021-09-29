ff01143449b975a7d1d8b3b0531323045ed35cc0
nyctonio <nyctonio.dev@gmail.com>
2021-09-29 23:53:18 +0530

    day-18

--- code/trees/trees-1.cpp
+++ code/trees/trees-1.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 #define endl "\n"
 
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+};
 using namespace std;
 int main()
 {
-
+    node *root = NULL;
     return 0;
 }
